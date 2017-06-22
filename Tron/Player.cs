e2f15173d6e7569ab5cc23cfd5ccf34e263b5ca0
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2017-06-22 17:02:07 +0100

    Addition of Cell and Grid objects to make life easier going forward

--- Tron/Player.cs
+++ Tron/Player.cs
@@ -11,65 +11,61 @@ class Player
 	private const string Up = "UP";
 	private const string Down = "DOWN";
 
-	private const int GridMaxX = 29;
-	private const int GridMaxY = 19;
-
+	private static readonly Grid TheGrid = new Grid(30, 20);
 
 	static void Main(string[] args)
-    {
-        string[] inputs;
+	{
+		string currentDirection = null;
 
-	    string currentDirection = null;
-		var closedCells = new HashSet<Point>();
+		// game loop
+		while (true)
+		{
+			var inputs = Console.ReadLine().Split(' ');
+			int numberOfPlayers = int.Parse(inputs[0]); // total number of players (2 to 4).
+			int myPlayerNumber = int.Parse(inputs[1]); // your player number (0 to 3).
 
-        // game loop
-        while (true)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int numberOfPlayers = int.Parse(inputs[0]); // total number of players (2 to 4).
-            int myPlayerNumber = int.Parse(inputs[1]); // your player number (0 to 3).
-            for (int i = 0; i < numberOfPlayers; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
+			for (int i = 0; i < numberOfPlayers; i++)
+			{
+				inputs = Console.ReadLine().Split(' ');
 
 				int x = int.Parse(inputs[2]); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
-                int y = int.Parse(inputs[3]); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+				int y = int.Parse(inputs[3]); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
 
-				
+
 				// Regardless of the player, add the current cell to a list of closed cells
-	            closedCells.Add(new Point(x, y));
+				TheGrid.AddClosedCell(x, y);
 
 
 				if (i == myPlayerNumber)
-	            {
-		            var newDirectionOptions = new Collection<string>();
+				{
+					var newDirectionOptions = new Collection<string>();
 
-		            if (y > 0 && currentDirection != Down && !closedCells.Contains(new Point(x, y - 1)))
-		            {
+					if (y > 0 && currentDirection != Down && !TheGrid.HasClosedCell(x, y - 1))
+					{
 						// Can move up
-			            newDirectionOptions.Add(Up);
-		            }
+						newDirectionOptions.Add(Up);
+					}
 
-					if (y < GridMaxY && currentDirection != Up && !closedCells.Contains(new Point(x, y + 1)))
-		            {
+					if (y < TheGrid.MaxY && currentDirection != Up && !TheGrid.HasClosedCell(x, y + 1))
+					{
 						// Can move down
-			            newDirectionOptions.Add(Down);
-		            }
+						newDirectionOptions.Add(Down);
+					}
 
-					if (x > 0 && currentDirection != Right && !closedCells.Contains(new Point(x - 1, y)))
-		            {
+					if (x > 0 && currentDirection != Right && !TheGrid.HasClosedCell(x - 1, y))
+					{
 						// Can move leftLeft;
-			            newDirectionOptions.Add(Left);
-		            }
+						newDirectionOptions.Add(Left);
+					}
 
-					if (x < GridMaxX && currentDirection != Left && !closedCells.Contains(new Point(x + 1, y)))
-		            {
+					if (x < TheGrid.MaxX && currentDirection != Left && !TheGrid.HasClosedCell(x + 1, y))
+					{
 						// Can move right
-			            newDirectionOptions.Add(Right);
-		            }
+						newDirectionOptions.Add(Right);
+					}
 
 					// Retain direction
-		            string newDirection = newDirectionOptions.OrderBy(o => Guid.NewGuid()).First();
+					string newDirection = newDirectionOptions.OrderBy(o => Guid.NewGuid()).First();
 
 					currentDirection = newDirection;
 
@@ -78,9 +74,88 @@ class Player
 				}
 			}
 
-        }
+		}
 
-    }
+	}
 
+}
+
+public class Grid
+{
+	public readonly int MaxX;
+	public readonly int MaxY;
+	public readonly int Width;
+	public readonly int Height;
+	public readonly HashSet<Cell> ClosedCells = new HashSet<Cell>();
+
+	public Grid(int width, int height)
+	{
+		Width = width;
+		Height = height;
+
+		MaxX = width - 1;
+		MaxY = height - 1;
+	}
+
+	public void AddClosedCell(int x, int y)
+	{
+		ClosedCells.Add(new Cell(x, y, this));
+	}
+
+	public bool HasClosedCell(int x, int y)
+	{
+		return ClosedCells.Contains(new Cell(x, y, this));
+	}
+}
+
+public class Cell
+{
+	public readonly int X;
+	public readonly int Y;
+
+	private readonly Grid _grid;
+
+	public Cell(int x, int y, Grid grid)
+	{
+		X = x;
+		Y = y;
+
+		_grid = grid;
+	}
+
+	public bool IsClosed()
+	{
+		return _grid.ClosedCells.Contains(this);
+	}
+
+	#region Overrides
+
+	public override string ToString()
+	{
+		return String.Format("{0},{1}", X, Y);
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (obj == null)
+			return false;
+
+		var coordinates = (Cell)obj;
+		return X == coordinates.X && Y == coordinates.Y;
+	}
+
+	public override int GetHashCode()
+	{
+		unchecked // Overflow is fine, just wrap
+		{
+			int hash = 17;
+			// Suitable nullity checks etc, of course :)
+			hash = hash * 23 + X.GetHashCode();
+			hash = hash * 23 + Y.GetHashCode();
+			return hash;
+		}
+	}
+
+	#endregion
 
 }
