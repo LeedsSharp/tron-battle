d9eec9c9c5ac8d189cb157379dc5f168b2a13374
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2017-06-22 20:16:00 +0100

    Add 4-way floodfill algorithm.

--- Tron/Player.cs
+++ Tron/Player.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Drawing;
 using System.Linq;
 
 class Player
@@ -47,48 +45,144 @@ class Player
 
 			for (int i = 0; i < numberOfPlayers; i++)
 			{
-				if (i == myPlayerNumber)
+				if (i != myPlayerNumber)
+					continue;
+
+				var floodFill = new FloodFillAlgorithm(TheGrid);
+				var immediateNeighbours = floodFill.NeighboursOf(currentCell, 30);
+				var nextCell = immediateNeighbours.OrderByDescending(n => n.Value).ThenBy(n => Guid.NewGuid()).First().Key;
+
+				string newDirection = null;
+
+				if (nextCell.X > currentCell.X)
 				{
-					var newDirectionOptions = new Collection<string>();
-
-					if (currentCell.Y > 0 && currentDirection != Down && !TheGrid.HasClosedCell(currentCell.X, currentCell.Y - 1))
-					{
-						// Can move up
-						newDirectionOptions.Add(Up);
-					}
-
-					if (currentCell.Y < TheGrid.MaxY && currentDirection != Up && !TheGrid.HasClosedCell(currentCell.X, currentCell.Y + 1))
-					{
-						// Can move down
-						newDirectionOptions.Add(Down);
-					}
-
-					if (currentCell.X > 0 && currentDirection != Right && !TheGrid.HasClosedCell(currentCell.X - 1, currentCell.Y))
-					{
-						// Can move leftLeft;
-						newDirectionOptions.Add(Left);
-					}
-
-					if (currentCell.X < TheGrid.MaxX && currentDirection != Left && !TheGrid.HasClosedCell(currentCell.X + 1, currentCell.Y))
-					{
-						// Can move right
-						newDirectionOptions.Add(Right);
-					}
-
-					// Retain direction
-					string newDirection = newDirectionOptions.OrderBy(o => Guid.NewGuid()).First();
-
-					currentDirection = newDirection;
-
-					// Set direction
-					Console.WriteLine(newDirection);
+					newDirection = Right;
 				}
-			}}
+				else if (nextCell.X < currentCell.X)
+				{
+					newDirection = Left;
+				}
+				else if (nextCell.Y > currentCell.Y)
+				{
+					newDirection = Down;
+				}
+				else
+				{
+					newDirection = Up;
+				}
+
+				// Set direction
+				Console.WriteLine(newDirection);
+			}
 
 		}
 
 	}
 
+}
+
+public class FloodFillAlgorithm
+{
+	private readonly Grid _grid;
+
+	private bool IncludeClosedCells { get; set; }
+
+	public FloodFillAlgorithm(Grid grid)
+	{
+		_grid = grid;
+	}
+
+	/// <summary>
+	/// Returns the immediate neighbouring cells
+	/// </summary>
+	public IEnumerable<Cell> NeighboursOf(Cell cell)
+	{
+		return NeighboursOf(cell, 1).Keys;
+	}
+
+	/// <summary>
+	/// Returns a dictionary of the immediate neighbouring cells and their associated floodfills
+	/// </summary>
+	public IDictionary<Cell, int> NeighboursOf(Cell cell, int depth)
+	{
+		// Find immediate neighbors
+		var immediateNeighbours = new HashSet<Cell>();
+		FindNeighbouringCellsOf(cell, 1, immediateNeighbours);
+
+		var ret = new Dictionary<Cell, int>();
+
+		if (depth == 1)
+		{
+			ret.Add(cell, immediateNeighbours.Count);
+		}
+		else
+		{
+			foreach (var neighbour in immediateNeighbours)
+			{
+				var neighbourFloodFill = new HashSet<Cell>();
+
+				FindNeighbouringCellsOf(neighbour, depth, neighbourFloodFill);
+
+				ret.Add(neighbour, neighbourFloodFill.Count);
+			}
+		}
+
+		return ret;
+	}
+
+	private void FindNeighbouringCellsOf(Cell cell, int depth, HashSet<Cell> set)
+	{
+		depth = depth - 1;
+
+		// Move right
+		var newCell = _grid.Cell(cell.X + 1, cell.Y);
+		if (newCell.X <= _grid.MaxX && !set.Contains(newCell) && (!newCell.IsClosed() || (IncludeClosedCells && newCell.IsClosed())))
+		{
+			set.Add(newCell);
+
+			if (depth > 0)
+			{
+				FindNeighbouringCellsOf(newCell, depth, set);
+			}
+		}
+
+		// Move down
+		newCell = _grid.Cell(cell.X, cell.Y + 1);
+		if (newCell.Y <= _grid.MaxY && !set.Contains(newCell) && (!newCell.IsClosed() || (IncludeClosedCells && newCell.IsClosed())))
+		{
+			set.Add(newCell);
+
+			if (depth > 0)
+			{
+				FindNeighbouringCellsOf(newCell, depth, set);
+			}
+		}
+
+		// Move left
+		newCell = _grid.Cell(cell.X - 1, cell.Y);
+		if (newCell.X >= 0 && !set.Contains(newCell) && (!newCell.IsClosed() || (IncludeClosedCells && newCell.IsClosed())))
+		{
+			set.Add(newCell);
+
+			if (depth > 0)
+			{
+				FindNeighbouringCellsOf(newCell, depth, set);
+			}
+		}
+
+		// Move up
+		newCell = _grid.Cell(cell.X, cell.Y - 1);
+		if (newCell.Y >= 0 && !set.Contains(newCell) && (!newCell.IsClosed() || (IncludeClosedCells && newCell.IsClosed())))
+		{
+			set.Add(newCell);
+
+			if (depth > 0)
+			{
+				FindNeighbouringCellsOf(newCell, depth, set);
+			}
+		}
+	}
+}
 
 public class Grid
 {
